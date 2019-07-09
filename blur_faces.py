b4ae299b05ccbe9b9a3b68063ca3b9b1da65b93b
Chih-Yuan Yang <yangchihyuan@gmail.com>
2019-07-09 09:49:45 +0000

    implement demo.py

--- blur_faces.py
+++ blur_faces.py
@@ -15,13 +15,13 @@ try:
     # Windows Import
     if platform == "win32":
         # Change these variables to point to the correct folder (Release/x64 etc.) 
-        sys.path.append(dir_path + '/../../python/openpose/Release');
+        sys.path.append(dir_path + '/../../python/openpose/Release')
         os.environ['PATH']  = os.environ['PATH'] + ';' + dir_path + '/../../x64/Release;' +  dir_path + '/../../bin;'
         import pyopenpose as op
     else:
         # Change these variables to point to the correct folder (Release/x64 etc.) 
         #sys.path.append('../../python');
-        sys.path.append('/usr/local/python');
+        sys.path.append('/usr/local/python')
         # If you run `make install` (default path is `/usr/local/python` for Ubuntu), you can also access the OpenPose/python module from there. This will install OpenPose and the python library at your desired installation path. Ensure that this is in your python path in order to use it.
         # sys.path.append('/usr/local/python')
         from openpose import pyopenpose as op
@@ -31,7 +31,8 @@ except ImportError as e:
 
 # Flags
 parser = argparse.ArgumentParser()
-parser.add_argument("--image_path", default="/4t/yangchihyuan/1226", required=True, help="image path")
+parser.add_argument("--image_path", default="/4t/yangchihyuan/keyframes", required=True, help="image path")
+parser.add_argument("--output_path", default="/4t/yangchihyuan/keyframes_blurred", required=True, help="image path")
 parser.add_argument("--GaussianKernelWidth", default=21, required=True, type=int, help="Gaussian kernel width")
 parser.add_argument("--model_directory", default="/home/yangchihyuan/openpose/models/", required=True, help="OpenPose model directory")
 args = parser.parse_args()
@@ -41,30 +42,9 @@ params = dict()
 params["model_folder"] = args.model_directory
 params[ "model_pose" ] = "COCO"
 
-# Add others in path?
-'''
-for i in range(0, len(args[1])):
-    curr_item = args[1][i]
-    if i != len(args[1])-1: next_item = args[1][i+1]
-    else: next_item = "1"
-    if "--" in curr_item and "--" in next_item:
-        key = curr_item.replace('-','')
-        if key not in params:  params[key] = "1"
-    elif "--" in curr_item and "--" not in next_item:
-        key = curr_item.replace('-','')
-        if key not in params: params[key] = next_item
-'''
-# Construct it from system arguments
-# op.init_argv(args[1])
-# oppython = op.OpenposePython()
-
-mypath = args.image_path
-outputpath = args.image_path + "_blurred"
-if not os.path.exists(outputpath):
-    os.makedirs(outputpath)
-
-file_list = [f for f in listdir(mypath) if isfile(join(mypath, f))]
-print( file_list)
+listOfFiles = list()
+for (dirpath, dirnames, filenames) in os.walk(args.image_path):
+    listOfFiles += [os.path.join(dirpath, file) for file in filenames]
 
 try:
     # Starting OpenPose
@@ -74,23 +54,14 @@ try:
 
     # Process Image
     datum = op.Datum()
-    for filename in file_list:
-        imageToProcess = cv2.imread(join(mypath,filename))
+    for filename in listOfFiles:
+        imageToProcess = cv2.imread(filename)
         mask = np.zeros_like(imageToProcess)
-        #mask = np.zeros([imageToProcess.shape[0], imageToProcess.shape[1]])
         datum.cvInputData = imageToProcess
         opWrapper.emplaceAndPop([datum])
 
-    # Analysize keypoints
-    #print(type(datum.faceRectangles))   #list
-    #print(str(datum.faceRectangles))    #empty
-
-#        print("Body keypoints: \n" + str(datum.poseKeypoints))    #numpy.ndarray, it becomes an int 2.0 
-    # Display Image
-#        cv2.imshow("OpenPose 1.5.0 - Tutorial Python API", datum.cvOutputData)
-#        cv2.waitKey(0)
-        print(datum.poseKeypoints.shape)
-        print(datum.poseKeypoints.size)
+#        print(datum.poseKeypoints.shape)
+#        print(datum.poseKeypoints.size)
         if datum.poseKeypoints.size == 1:
             print("no person")
         else:
@@ -106,39 +77,25 @@ try:
             leftear_y = datum.poseKeypoints[0][17][1]
             print("leftear_x leftear_y " + str(leftear_x) + " " + str(leftear_y))
 
-            if nose_x == 0 or rightear_x == 0 or leftear_x == 0:
+            if nose_x == 0:
                 print("no face")
             else:
-                roi_left = int(rightear_x)
-                roi_right = int(leftear_x)
-                roi_width = roi_right - roi_left
-                roi_bottom = int(nose_y - 0.5 * roi_width)
-                if roi_bottom < 0:
-                    roi_bottom = 0
-                roi_top = int(nose_y + 0.5 * roi_width)
-                if roi_top > 479:
-                    roi_top = 479
+                if rightear_x != 0 and leftear_x != 0:
+                    roi_width = leftear_x - rightear_x
+                elif rightear_x != 0:
+                    roi_width = 2*( nose_x - rightear_x )
+                elif leftear_x != 0:
+                    roi_width = 2*(leftear_x - nose_x)
 
                 blurred_image = cv2.GaussianBlur(imageToProcess, (args.GaussianKernelWidth,args.GaussianKernelWidth), 0)
-#                print(blurred_image.shape)
- #               cv2.imshow("blurred_image", blurred_image)   #the image is very small. why?
- #               cv2.waitKey(0)
-
-#                print( [roi_left, roi_right, roi_bottom, roi_top])
-
                 mask=cv2.ellipse(mask, center=(nose_x, nose_y), axes=(int(0.5*roi_width),int(0.5*1.3*roi_width)), angle=0, startAngle=0, endAngle=360, color=(255,255,255), thickness=-1)
-#                cv2.imshow("mask", mask)
-#                cv2.waitKey(0)
-
-#                blurred_face = np.bitwise_and(blurred_image, mask)
                 imageToProcess[np.where(mask==255)]=blurred_image[np.where(mask==255)]
-#                imageToProcess[roi_bottom:roi_top,roi_left:roi_right] = blurred_image[roi_bottom:roi_top,roi_left:roi_right]
-#                cv2.imshow("final_image", imageToProcess)
-#                cv2.waitKey(0)
-                # Display Image
-#                cv2.imshow("OpenPose 1.5.0 - Tutorial Python API", datum.cvOutputData)
-#                cv2.waitKey(0)
-                cv2.imwrite( join(outputpath,filename), imageToProcess )
+                output_filename_full = str.replace(filename, args.image_path,args.output_path)
+                dirname = os.path.dirname(output_filename_full)
+                if not os.path.exists(dirname):
+                    os.makedirs(dirname)
+
+                cv2.imwrite( output_filename_full, imageToProcess )
 
 except Exception as e:
     print(e)
--- demo.py
+++ demo.py
@@ -1,2 +1,155 @@
 #Chih-Yuan Yang 2019/7/5
+import sys
+import cv2
+import os
+from sys import platform
+import argparse
+from os import listdir
+from os.path import isfile, join
+import numpy as np
+from math import pow, sqrt
 
+# Import Openpose Ubuntu
+sys.path.append('/usr/local/python')
+from openpose import pyopenpose as op
+
+def str2bool(v):
+    if isinstance(v, bool):
+       return v
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):
+        return True
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+        return False
+    else:
+        raise argparse.ArgumentTypeError('Boolean value expected.')
+
+
+# Flags
+parser = argparse.ArgumentParser()
+parser.add_argument("--image_path", default="/home/yangchihyuan/RobotVideoSummary_Summarization/frames/20190503", help="image path")
+parser.add_argument("--output_path", default="/home/yangchihyuan/RobotVideoSummary_Summarization/frames/20190503_classified", help="image path")
+parser.add_argument("--model_directory", default="/home/yangchihyuan/openpose/models/", help="OpenPose model directory")
+parser.add_argument("--filelist", default="/home/yangchihyuan/RobotVideoSummary_Summarization/filelist.txt", required=False)
+parser.add_argument("--usefilelist", default=False, type=str2bool, required=False, help="if False, all files in the image_path will be used. Otherwise, only files in filelist will be used.")
+parser.add_argument("--blurred_var_threshold", default=200, required=False)
+parser.add_argument("--distance_center_to_eye_threshold", default=60)
+args = parser.parse_args()
+
+# Custom Params (refer to include/openpose/flags.hpp for more parameters)
+params = dict()
+params["model_folder"] = args.model_directory
+params[ "model_pose" ] = "COCO"
+#params["render_threshold"] = 0.7
+
+listOfFiles = list()
+if args.usefilelist and args.filelist is not None:
+    with open(args.filelist ,'r') as f:
+        content = f.readlines()
+    listOfFiles += [os.path.join(args.image_path, file.strip()) for file in content]
+else:
+    for (dirpath, dirnames, filenames) in os.walk(args.image_path):
+        listOfFiles += [os.path.join(dirpath, file) for file in filenames]
+
+# Starting OpenPose
+opWrapper = op.WrapperPython()
+opWrapper.configure(params)
+opWrapper.start()
+
+# Process Image
+datum = op.Datum()
+for filename in listOfFiles:
+    imageToProcess = cv2.imread(filename)
+    mask = np.zeros_like(imageToProcess)
+    datum.cvInputData = imageToProcess
+    opWrapper.emplaceAndPop([datum])
+
+    basename = os.path.basename(filename)
+    print(basename)
+    if datum.poseKeypoints.size == 1:
+        print("no person")
+    else:
+        for person_idx in range(0, datum.poseKeypoints.shape[0]):
+            nose_x = datum.poseKeypoints[person_idx][0][0]
+            nose_y = datum.poseKeypoints[person_idx][0][1]
+            nose_prob = datum.poseKeypoints[person_idx][0][2]
+            print("nose_x nose_y " + str(nose_x) + " " + str(nose_y) + " prob " + str(nose_prob))
+
+            center_prob = datum.poseKeypoints[person_idx][1][2]
+
+            lefteye_x = datum.poseKeypoints[person_idx][14][0]
+            lefteye_y = datum.poseKeypoints[person_idx][14][1]
+            lefteye_prob = datum.poseKeypoints[person_idx][14][2]
+
+            righteye_x = datum.poseKeypoints[person_idx][15][0]
+            righteye_y = datum.poseKeypoints[person_idx][15][1]
+            righteye_prob = datum.poseKeypoints[person_idx][15][2]
+
+
+            # rightear_x = datum.poseKeypoints[person_idx][16][0]
+            # rightear_y = datum.poseKeypoints[person_idx][16][1]
+            # rightear_prob = datum.poseKeypoints[person_idx][16][2]
+            # print("rightear_x rightear_y " + str(rightear_x) + " " + str(rightear_y) + " prob " + str(rightear_prob))
+
+            # leftear_x = datum.poseKeypoints[person_idx][17][0]
+            # leftear_y = datum.poseKeypoints[person_idx][17][1]
+            # leftear_prob = datum.poseKeypoints[person_idx][17][2]
+            # print("leftear_x leftear_y " + str(leftear_x) + " " + str(leftear_y) + " prob " + str(leftear_prob))
+
+            if center_prob == 0:
+                output_directory = os.path.join(args.output_path, "case1_no_center")
+                if not os.path.exists(output_directory):
+                    os.makedirs(output_directory)
+                cv2.imwrite( os.path.join( output_directory, basename ), datum.cvOutputData )
+            elif lefteye_prob <= 0.01 and righteye_prob <= 0.01:
+                output_directory = os.path.join(args.output_path, "case2_two_eyes_invisible")
+                if not os.path.exists(output_directory):
+                    os.makedirs(output_directory)
+                cv2.imwrite( os.path.join( output_directory, basename ), datum.cvOutputData )
+            elif nose_x <= 213 or nose_x >=427:
+                output_directory = os.path.join(args.output_path, "case3_not_at_center")
+                if not os.path.exists(output_directory):
+                    os.makedirs(output_directory)
+                cv2.imwrite( os.path.join( output_directory, basename ), datum.cvOutputData )
+            else:
+                #test the size
+                distance_center_to_lefteye = 0
+                if nose_prob > 0 and lefteye_prob > 0:
+                    distance_center_to_lefteye = sqrt(pow(nose_x - lefteye_x,2) + pow(nose_y - lefteye_y, 2))
+                distance_center_to_righteye = 0
+                if nose_prob > 0 and righteye_prob > 0:
+                    distance_center_to_righteye = sqrt(pow(nose_x - righteye_x,2) + pow(nose_y - righteye_y, 2))
+                max_distance = max(distance_center_to_lefteye, distance_center_to_righteye)
+                if max_distance < args.distance_center_to_eye_threshold:
+                    output_directory = os.path.join(args.output_path, "case4_small_size")
+                    if not os.path.exists(output_directory):
+                        os.makedirs(output_directory)
+                    cv2.imwrite( os.path.join( output_directory, basename ), datum.cvOutputData )
+                
+
+            #check image blur
+            image_gray = cv2.cvtColor(imageToProcess, cv2.COLOR_BGR2GRAY)
+            image_laplacian = cv2.Laplacian(image_gray, -1, ksize=1, scale=1, delta=0)
+            var = np.var(image_laplacian)
+            print( "var: " + str(var))
+            if var < args.blurred_var_threshold:
+                print("blurred")
+                output_directory = os.path.join(args.output_path, "case6_blurred")
+                if not os.path.exists(output_directory):
+                    os.makedirs(output_directory)
+                cv2.imwrite( os.path.join( output_directory, basename ), datum.cvOutputData )
+                continue
+
+
+            # cv2.imshow("Test", image_laplacian)
+            # cv2.waitKey(0)
+
+            # else:
+            output_filename_full = str.replace(filename, args.image_path,args.output_path)
+            dirname = os.path.dirname(output_filename_full)
+            if not os.path.exists(dirname):
+                os.makedirs(dirname)
+
+            output_directory = os.path.join(args.output_path, "effective")
+            if not os.path.exists(output_directory):
+                os.makedirs(output_directory)
+            cv2.imwrite( os.path.join( output_directory, basename ), datum.cvOutputData )
